a8fc41eb37d317feb82a2846666e0a6c5c095052
alita <1906321518@qq.com>
2019-11-06 12:39:31 +0800

    update

--- learnDjango/projects/migrations/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# @Time    : 2019/9/27 22:25
-# @Author  : icon
-# @File    : __init__.py.py
-
-
--- learnDjango/projects/tests.py
+++
@@ -1,6 +0,0 @@
-from os import environ
-
-from django.test import TestCase
-
-# Create your tests here.
-print(environ.get('CYGWIN'))
